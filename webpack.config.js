7bb64db6a9d876fdf86420e9b5d7c8fa3595f0a5
hcpioquinto <harveycharles.pioquinto@devpartners.co>
2020-09-07 13:24:57 +0800

    Init work

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,35 @@
 
+const path = require("path");
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  devtool: "source-map",
+  entry: "./src/js/index.js",
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, './build/static'),
+    publicPath: path.resolve(__dirname, './build/static')
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + "/public/index.html",
+      filename: "../index.html",
+      inject: "body",
+    }),
+    new webpack.NamedModulesPlugin()
+  ],
+};
